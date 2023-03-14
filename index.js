ead2454a29207c39dba926887903b9753680ae3b
Warren Zhang <warrenzhang17@gmail.com>
2023-03-13 21:54:55 -0400

    card countdown

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(names, eventName) {
+    const messages = [];
+    
+    for (let i = 0; i < names.length; i++) {
+    const message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+    messages.push(message);
+    }
+    
+    return messages;
+    }
+
+function countDown(num) {
+    for (let i = num; i >= 0; i--) {
+    console.log(i);
+    }
+}
